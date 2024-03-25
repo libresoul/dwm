a0e2f9be40158ac0c38da587f34164ce619ee909
libresoul <dragoofficial991@gmail.com>
2024-03-26 04:45:01 +0530

    equal width for master and slave

--- config.h
+++ config.h
@@ -57,7 +57,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int statusall   = 1;    /* 1 means status is shown in all bars, not just active monitor */
